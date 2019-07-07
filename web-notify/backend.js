4b2d909bbe53a44cf9e4d5298f82e5f141b88653
zhixianMatthewZhao <46915203+zhixianMatthewZhao@users.noreply.github.com>
2019-07-06 19:34:37 -0700

    Add files via upload

---
+++ web-notify/backend.js
@@ -0,0 +1,19 @@
+const ping = require('ping');
+const Discord = require('discord.js');
+const token = "NTgwNTU3NTY5OTcwNTM2NDYw.XRFHMw.y9WVxLftv6MnPsu7tmwMTeh5h6g";
+const client = new Discord.Client();
+client.on("message", (message)=>{
+  if (message.content.startsWith('$status')){
+  var hosts = ['10.20.0.60', '10.20.0.70', '10.20.0.80','10.20.0.90','10.20.0.100'];
+  hosts.forEach(function(host){
+    ping.sys.probe(host, function(isAlive){
+      var msg = isAlive ? 'host ' + host + ' is alive' : 'host ' + host + ' is dead';
+      console.log(msg);
+      msg+="\r\n";
+      message.channel.send(msg);
+      });
+
+    });}
+  });
+
+client.login(token)
